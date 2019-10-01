f167cb5e304b42a1a0b2602d2bfe0effe6640acb
Nikita Anshakov <n.anshakov@crpt.ru>
2019-10-01 17:22:45 +0300

    Vk SDK и минимальная авторизвция

--- src/main/java/com/nanshakov/vcs2/controllers/ApiController.java
+++ src/main/java/com/nanshakov/vcs2/controllers/ApiController.java
@@ -3,7 +3,6 @@ package com.nanshakov.vcs2.controllers;
 import com.nanshakov.vcs2.services.Auth;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,14 +21,13 @@ public class ApiController {
     private Auth auth;
 
     @PostMapping("/api/start")
-    public static String startUp(@CookieValue(value = "GKey") String gKey, @RequestBody List<String> body,
-                                 HttpServletResponse response) {
+    public static String start(@RequestBody List<String> body, HttpServletResponse response) {
 //        body.forEach(l -> ProcessingCache.getInstance().push(gKey, new ProcessingStatusDto(gKey, l)));
-        return "/getProcessing";
+        return "getProcessing";
     }
 
     @GetMapping("/api/getResult")
-    public static Map getResult(@CookieValue(value = "GKey") String gKey, HttpServletResponse response) {
+    public static Map getResult(String gKey, HttpServletResponse response) {
         //return Collections.singletonMap("response", ProcessingCache.getInstance().get(gKey));
         return new HashMap();
     }
--- src/main/java/com/nanshakov/vcs2/services/Auth.java
+++ src/main/java/com/nanshakov/vcs2/services/Auth.java
@@ -1,8 +1,46 @@
 package com.nanshakov.vcs2.services;
 
+import com.vk.api.sdk.client.TransportClient;
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.exceptions.ApiException;
+import com.vk.api.sdk.exceptions.ClientException;
+import com.vk.api.sdk.httpclient.HttpTransportClient;
+import com.vk.api.sdk.objects.UserAuthResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.log4j.Log4j2;
+
 @Service
+@Log4j2
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class Auth {
 
+    @Value("${api.appId}")
+    Integer appId;
+    @Value("${api.secret}")
+    String secret;
+    @Value("${api.redirectUrl}")
+    String redirectUrl;
+    private String token;
+
+    String getCode() {
+        return "55cd599cd21af55545";
+    }
+
+    public String getToken(String code) throws ClientException, ApiException {
+        if (token == null) {
+            TransportClient transportClient = new HttpTransportClient();
+            VkApiClient vk = new VkApiClient(transportClient);
+            UserAuthResponse authResponse = vk.oAuth()
+                    .userAuthorizationCodeFlow(appId, secret, redirectUrl, code)
+                    .execute();
+            token = authResponse.getAccessToken();
+            return token;
+        }
+        return token;
+    }
 }
